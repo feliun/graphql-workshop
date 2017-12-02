426677be982e6d84fdeed57f386dffdb0b97190e
feliun <felipe.polo@gmail.com>
2017-12-02 01:53:53 +0100

    Changing topic to courses and students

--- mongo/setup.sh
+++ mongo/setup.sh
@@ -6,6 +6,7 @@ echo "username: $MONGO_DB_APP_USERNAME"
 echo "password: $MONGO_DB_APP_PASSWORD"
 
 mongo admin -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "db.getSiblingDB('$MONGO_DB_APP_DATABASE').createUser({user: '$MONGO_DB_APP_USERNAME', pwd: '$MONGO_DB_APP_PASSWORD', roles: [{role: 'readWrite', db: '$MONGO_DB_APP_DATABASE'}]});"
-mongoimport -u $MONGO_DB_APP_USERNAME -p $MONGO_DB_APP_PASSWORD --db $MONGO_DB_APP_DATABASE --collection books --type json --file /data.json --jsonArray
+mongoimport -u $MONGO_DB_APP_USERNAME -p $MONGO_DB_APP_PASSWORD --db $MONGO_DB_APP_DATABASE --collection courses --type json --file /courses.json --jsonArray
+mongoimport -u $MONGO_DB_APP_USERNAME -p $MONGO_DB_APP_PASSWORD --db $MONGO_DB_APP_DATABASE --collection students --type json --file /students.json --jsonArray
 
 echo "Mongo db and users created."
