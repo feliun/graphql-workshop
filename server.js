35513b77645881819eb97285619c86feb6040cf9
feliun <felipe.polo@gmail.com>
2017-12-02 18:49:36 +0100

    Preparing first example

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
-const cors = require('cors');
+const graphqlHTTP = require('express-graphql');
+const { GraphQLSchema, GraphQLObjectType } = require('graphql');
+
 const port = process.env.PORT || 8080;
+
 const app = express();
 
+const moto = 'May the force be with you';
+const motoAsObject = { moto };
+
+const schema = new GraphQLSchema({
+  // query operations defined
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      // define here a 'moto' operation which will return the moto as a string
+      moto: { },
+      // define here a 'motoAsObject' operation which will return motoAsObject
+      motoAsObject: { }
+    }
+  })
+});
+
 app.use(
-  '/test',
-  cors(),
-  (req, res) => res.json({ ok: true })
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
 );
 
-const listen = () => {
-  app.listen(port);
-  console.log(`Express app started on port ${port}`);
-};
-
-listen();
+app.listen(port);
+console.log(`Server listening at localhost:${port}`);
