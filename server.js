033a632ca4fd975dbf5bd2c230bd8fe869e9f449
feliun <felipe.polo@gmail.com>
2017-12-02 18:51:16 +0100

    Resolving first example

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { GraphQLSchema, GraphQLObjectType } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 
 const port = process.env.PORT || 8080;
 
@@ -10,14 +10,22 @@ const moto = 'May the force be with you';
 const motoAsObject = { moto };
 
 const schema = new GraphQLSchema({
-  // query operations defined
   query: new GraphQLObjectType({
     name: 'Query',
     fields: {
-      // define here a 'moto' operation which will return the moto as a string
-      moto: { },
-      // define here a 'motoAsObject' operation which will return motoAsObject
-      motoAsObject: { }
+      moto: {
+        type: GraphQLString,
+        resolve: () => moto
+      },
+      motoAsObject: {
+        type: new GraphQLObjectType({
+          name: 'motoAsObject',
+          fields: {
+            moto: { type: GraphQLString }
+          }
+        }),
+        resolve: () => motoAsObject
+      }
     }
   })
 });
