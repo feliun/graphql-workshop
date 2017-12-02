520317be2c7a951cd7cdadbf1a7325fe1252f8e6
feliun <felipe.polo@gmail.com>
2017-12-02 19:13:04 +0100

    Films defined

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { GraphQLSchema, GraphQLObjectType } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = require('graphql');
 
 const port = process.env.PORT || 8080;
 
@@ -9,14 +9,50 @@ const app = express();
 const FILMS = require('./mongo/films.json');
 const CHARACTERS = require('./mongo/characters.json');
 
-const Film = // ...
-const Character = // ...
+const Film = new GraphQLObjectType({
+  name: 'Film',
+  description: 'A Star Wars film',
+  fields: {
+    title: { type: GraphQLString },
+    episode_id: { type: GraphQLInt },
+    opening_crawl: { type: GraphQLString },
+    director: { type: GraphQLString },
+    producer: { type: GraphQLString },
+    release_date: { type: GraphQLString },
+    characters: { type: new GraphQLList(GraphQLString) },
+    planets: { type: new GraphQLList(GraphQLString) },
+    starships: { type: new GraphQLList(GraphQLString) },
+    vehicles: { type: new GraphQLList(GraphQLString) },
+    species: { type: new GraphQLList(GraphQLString) },
+    created: { type: GraphQLString },
+    edited: { type: GraphQLString },
+    url: { type: GraphQLString },
+    desc: { type: new GraphQLList(GraphQLString) }
+  }
+});
+
+// const Character = // ...
 
 // TODO: define a schema with operations to
 // 1. get one film by id
 // 2. get a character by name
 // 3. Get all films
-const schema = // ...
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      film: {
+        type: Film,
+        args: { filmId: { type: GraphQLInt } },
+        resolve: (root, { filmId }, context) => {
+          if (!filmId) throw new Error('A filmId needs to be provided to get a film!');
+          return FILMS.find((film) => film.episode_id === filmId);
+        },
+      }
+    }
+  })
+});
 
 app.use(
   '/graphql',
