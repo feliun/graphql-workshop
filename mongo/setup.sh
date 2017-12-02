9ecf572a6eefe659ac8a34851d42072dd91d9fd3
feliun <felipe.polo@gmail.com>
2017-12-02 01:43:32 +0100

    Docker compose to create a seeded mongo instance

---
+++ mongo/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "Creating mongo db and users..."
+echo "Creating new mongo db: $MONGO_DB_APP_DATABASE"
+echo "Creating new mongo user"
+echo "username: $MONGO_DB_APP_USERNAME"
+echo "password: $MONGO_DB_APP_PASSWORD"
+
+mongo admin -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "db.getSiblingDB('$MONGO_DB_APP_DATABASE').createUser({user: '$MONGO_DB_APP_USERNAME', pwd: '$MONGO_DB_APP_PASSWORD', roles: [{role: 'readWrite', db: '$MONGO_DB_APP_DATABASE'}]});"
+mongoimport -u $MONGO_DB_APP_USERNAME -p $MONGO_DB_APP_PASSWORD --db $MONGO_DB_APP_DATABASE --collection books --type json --file /data.json --jsonArray
+
+echo "Mongo db and users created."
