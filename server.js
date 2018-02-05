e89f11d72899f6eaa561c229551edd43a1a3749c
feliun <felipe.polo@gmail.com>
2018-02-05 23:35:23 +0100

    Solving problem from 0

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { GraphQLSchema, GraphQLObjectType } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 const moto = 'May the force be with you';
-const character = { name: "Luke Skywalker" };
+const character = {
+  name: "Luke Skywalker",
+  height: 172
+};
 
 const schema = new GraphQLSchema({
   // query operations defined
   query: new GraphQLObjectType({
     name: 'Query',
     fields: {
-      // define here a 'moto' operation which will return the moto as a string
-      mainMoto: { },
-      // define here a 'motoAsObject' operation which will return motoAsObject
-      mainCharacter: { }
+      mainMoto: {
+        type: GraphQLString,
+        resolve: () => moto
+      },
+      mainCharacter: {
+        type: new GraphQLObjectType({
+          name: 'mainCharacter',
+          fields: {
+            name: { type: GraphQLString },
+            height: { type: GraphQLInt }
+          }
+        }),
+        resolve: () => character
+      }
     }
   })
 });
