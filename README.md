f9b27c6df63cf2c150ff602bcaf40883a9e1c908
feliun <felipe.polo@gmail.com>
2018-02-06 22:38:27 +0100

    README explained: use mongo

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # graphql-workshop
 
-**TODO:** create a basic graphQL endpoint serving a string and an object
+**TODO:** define a schema with operations to
+1. get one film by id
+2. get a character by name
+3. get all films
--- server.js
+++ server.js
@@ -1,47 +1,48 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+const mongodb = require('mongodb');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = require('graphql');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-const moto = 'May the force be with you';
-const character = {
-  name: "Luke Skywalker",
-  height: 172
-};
+const Film = //...
+
+const Character = //...
 
 const schema = new GraphQLSchema({
-  // query operations defined
   query: new GraphQLObjectType({
     name: 'Query',
     fields: {
-      mainMoto: {
-        type: GraphQLString,
-        resolve: () => moto
-      },
-      mainCharacter: {
-        type: new GraphQLObjectType({
-          name: 'mainCharacter',
-          fields: {
-            name: { type: GraphQLString },
-            height: { type: GraphQLInt }
-          }
-        }),
-        resolve: () => character
-      }
+
     }
   })
 });
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
-);
 
-app.listen(port);
-console.log(`Server listening at localhost:${port}`);
+const options = {
+  auth: {
+    user: process.env.MONGO_DB_APP_USERNAME || 'node',
+    password: process.env.MONGO_DB_APP_PASSWORD || 'node'
+  },
+  keepAlive: true,
+  reconnectTries: 30,
+  socketTimeoutMS: 0
+};
+
+mongodb.connect('mongodb://127.0.0.1/starwars', options)
+  .then((mongo) => {
+    app.listen(port);
+    console.log('Connected to mongo DB!');
+    app.use(
+      '/graphql',
+      graphqlHTTP({
+        schema,
+        context: { mongo },
+        graphiql: true
+      })
+    );
+    console.log(`Server listening at localhost:${port}`);
+  })
+  .catch(console.error);
