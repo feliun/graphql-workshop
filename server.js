4e32d0ce8abc58c2669cf1cf8c709ec7193a872b
feliun <felipe.polo@gmail.com>
2018-02-09 15:06:07 +0000

    Solving SWAPI models in a JS way

--- server.js
+++ server.js
@@ -1,21 +1,76 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongodb = require('mongodb');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList, GraphQLNonNull } = require('graphql');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-const Film = //...
+const Film = new GraphQLObjectType({
+  name: 'Film',
+  description: 'A Star Wars film',
+  fields: {
+    title: { type: GraphQLString },
+    episode_id: { type: GraphQLInt },
+    opening_crawl: { type: GraphQLString },
+    director: { type: GraphQLString },
+    producer: { type: GraphQLString },
+    release_date: { type: GraphQLString },
+    characters: { type: new GraphQLList(GraphQLString) },
+    planets: { type: new GraphQLList(GraphQLString) },
+    starships: { type: new GraphQLList(GraphQLString) },
+    vehicles: { type: new GraphQLList(GraphQLString) },
+    species: { type: new GraphQLList(GraphQLString) },
+    created: { type: GraphQLString },
+    edited: { type: GraphQLString },
+    url: { type: GraphQLString },
+    desc: { type: new GraphQLList(GraphQLString) }
+  }
+});
 
-const Character = //...
+const Character = new GraphQLObjectType({
+  name: 'Character',
+  description: 'A Star Wars character',
+  fields: {
+    name: { type: GraphQLString },
+    height: { type: GraphQLString },
+    mass: { type: GraphQLString },
+    hair_color: { type: GraphQLString },
+    skin_color: { type: GraphQLString },
+    eye_color: { type: GraphQLString },
+    birth_year: { type: GraphQLString },
+    gender: { type: GraphQLString },
+    homeworld: { type: GraphQLString },
+    films: { type: new GraphQLList(GraphQLString) },
+    species: { type: new GraphQLList(GraphQLString) },
+    vehicles: { type: new GraphQLList(GraphQLString) },
+    starships: { type: new GraphQLList(GraphQLString) },
+    created: { type: GraphQLString },
+    edited: { type: GraphQLString },
+    url: { type: GraphQLString },
+    desc: { type: new GraphQLList(GraphQLString) }
+  }
+});
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
     fields: {
-
+      films: {
+        type: new GraphQLList(Film),
+        resolve: (root, args, context) => context.mongo.collection('films').find({}).toArray()
+      },
+      film: {
+        type: Film,
+        args: { filmId: { type: new GraphQLNonNull(GraphQLInt) } },
+        resolve: (root, { filmId }, context) => context.mongo.collection('films').findOne({ episode_id: filmId })
+      },
+      character: {
+        type: Character,
+        args: { name: { type: new GraphQLNonNull(GraphQLString) } },
+        resolve: (root, { name }, context) => context.mongo.collection('characters').findOne({ name })
+      }
     }
   })
 });
